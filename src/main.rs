896c4d8ed008d01681565b99fb93734818d44b11
kizianov.ao <kizianov.ao@svrauto.ru>
2025-11-18 12:44:58 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use dotenv::dotenv;
 use futures_util::{SinkExt, StreamExt};
 use log::{error, info};
 use std::env;
+use tokio::sync::mpsc;
 use tokio::time::{Duration, interval};
 use tokio_tungstenite::{connect_async, tungstenite::Message};
 mod api {
@@ -10,28 +11,48 @@ mod api {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     env_logger::init();
     dotenv().ok();
+
+    let (tx, mut rx) = mpsc::channel::<String>(1000);
+
+    let handler = tokio::spawn(async move {
+        while let Some(msg) = rx.recv().await {
+            println!("Processing: {}", msg);
+            // Здесь можно добавить реальную обработку: парсинг, запись в БД и т.д.
+        }
+        info!("Message handler finished");
+    });
+
     let mut ws_url: String = "".to_string();
 
-    match api::requests::KuCoinClient::new("https://api.kucoin.com".to_string()) {
-        Ok(client) => match client.api_v1_bullet_public().await {
-            Ok(bullet_public) => {
-                info!("Public {:?}", bullet_public);
-                for instance in bullet_public.data.instanceServers.iter() {
-                    ws_url = format!("{}?token={}", instance.endpoint, bullet_public.data.token);
-                    break;
+    let ws_url = {
+        match api::requests::KuCoinClient::new("https://api.kucoin.com".to_string()) {
+            Ok(client) => match client.api_v1_bullet_public().await {
+                Ok(bullet_public) => {
+                    info!("Public {:?}", bullet_public);
+                    bullet_public
+                        .data
+                        .instanceServers
+                        .first()
+                        .map(|s| format!("{}?token={}", s.endpoint, bullet_public.data.token))
+                        .unwrap_or_default()
                 }
-            }
+                Err(e) => {
+                    error!("Ошибка при выполнении запроса: {}", e);
+                    return Err(e);
+                }
+            },
             Err(e) => {
-                error!("Ошибка при выполнении запроса: {}", e)
+                error!("Ошибка при выполнении запроса: {}", e);
+                return Err(e.into());
             }
-        },
-        Err(e) => {
-            error!("Ошибка при выполнении запроса: {}", e)
         }
     };
+    if ws_url.is_empty() {
+        return Err("Не удалось получить WebSocket URL".into());
+    }
     // match api::requests::KuCoinClient::new("https://api.kucoin.com".to_string()) {
     //     Ok(client) => match client.api_v1_bullet_private().await {
     //         Ok(bullet_public) => {
@@ -67,7 +88,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             msg = read.next() => {
                 match msg {
                     Some(Ok(Message::Text(text))) => {
-                        info!("WebSocket message: {}", text);
+                        if tx.send(text.to_string()).await.is_err() {
+                            break;
+                        }
                     }
                     Some(Ok(Message::Ping(data))) => {
                         write.send(Message::Pong(data)).await?;
@@ -89,6 +112,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
     }
+    drop(tx);
+
+    let _ = handler.await;
+    info!("Application shutdown complete");
 
     Ok(())
 }
