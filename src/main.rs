faa9feda692d5435eb42251a10852ee395b90881
BizKey <toonychannel@gmail.com>
2025-12-30 20:48:38 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -683,6 +683,41 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             insert_db_error(&pool, &exchange, &msg).await;
         }
     }
+    match api::requests::sent_account_transfer("SEI", "200", "INTERNAL", "MARGIN_V2", "MARGIN").await
+    {
+        Ok(_) => {}
+        Err(e) => {
+            let msg: String = format!(
+                "Failed send {} to TRADE from MARGIN on {} {}",
+                &account.currency,
+                &available.to_string(),
+                e
+            );
+            error!("{}", msg);
+            insert_db_error(&pool, &exchange, &msg).await;
+        }
+    }
+    match api::requests::sent_account_transfer(
+        "USDT",
+        "11.4044156",
+        "INTERNAL",
+        "MARGIN_V2",
+        "MARGIN",
+    )
+    .await
+    {
+        Ok(_) => {}
+        Err(e) => {
+            let msg: String = format!(
+                "Failed send {} to TRADE from MARGIN on {} {}",
+                &account.currency,
+                &available.to_string(),
+                e
+            );
+            error!("{}", msg);
+            insert_db_error(&pool, &exchange, &msg).await;
+        }
+    }
     loop {
         let mut all_asset_transfer: bool = true;
         match api::requests::get_all_margin_accounts().await {
