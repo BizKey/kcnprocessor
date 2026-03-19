2e1ce5313a69a9e23efa9db3bfd702424cf8e2c7
BizKey <toonychannel@gmail.com>
2026-03-19 23:28:58 +0300

    .

--- src/api/db.rs
+++ src/api/db.rs
@@ -181,6 +181,42 @@ pub async fn clear_exit_tp_id_bot_by_entry_id(pool: &sqlx::PgPool, exchange: &st
         insert_db_error(pool, exchange, &err_msg).await;
     }
 }
+pub async fn update_exit_tp_id_bot_by_entry_id(
+    pool: &sqlx::PgPool,
+    exchange: &str,
+    entry_id: &str,
+    exit_tp_id: &str,
+) {
+    if let Err(e) = sqlx::query("UPDATE bots SET exit_tp_id = $1 updated_at = CURRENT_TIMESTAMP WHERE entry_id = $1 AND exchange = $2;")
+        .bind(exit_tp_id)
+        .bind(entry_id)
+        .bind(exchange)
+        .execute(pool)
+        .await
+    {
+        let err_msg = format!("Failed update exit_tp_id:{} by entry_id:{} for bots: {}",exit_tp_id,entry_id, e);
+        error!("{}", err_msg);
+        insert_db_error(pool, exchange, &err_msg).await;
+    }
+}
+pub async fn update_exit_sl_id_bot_by_entry_id(
+    pool: &sqlx::PgPool,
+    exchange: &str,
+    entry_id: &str,
+    exit_sl_id: &str,
+) {
+    if let Err(e) = sqlx::query("UPDATE bots SET exit_sl_id = $1 updated_at = CURRENT_TIMESTAMP WHERE entry_id = $1 AND exchange = $2;")
+        .bind(exit_sl_id)
+        .bind(entry_id)
+        .bind(exchange)
+        .execute(pool)
+        .await
+    {
+        let err_msg = format!("Failed update exit_sl_id:{} by entry_id:{} for bots: {}",exit_sl_id,entry_id, e);
+        error!("{}", err_msg);
+        insert_db_error(pool, exchange, &err_msg).await;
+    }
+}
 pub async fn clear_orders_ids_for_bots(pool: &sqlx::PgPool, exchange: &str) {
     if let Err(e) = sqlx::query("UPDATE bots SET entry_id = NULL, exit_tp_id = NULL, exit_sl_id = NULL, updated_at = CURRENT_TIMESTAMP WHERE exchange = $1;")
         .bind(exchange)
