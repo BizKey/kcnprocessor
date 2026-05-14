dd49f3d6cdaba9a1ce639f9f50cd4c6ba6247f9e
kizianov.ao <kizianov.ao@svrauto.ru>
2026-05-14 12:29:28 +0000

    .

--- src/logic.rs
+++ src/logic.rs
@@ -619,13 +619,15 @@ pub async fn handle_trade_order_event(order: OrderData, pool: &sqlx::Pool<sqlx::
             let msg: String = format!("Failed parse quote_increment: {} {}", symbol_info.quote_increment, e);
             log::error!("{}", msg);
             match insert_db_error(pool, exchange, &msg).await {
-                Ok(_) => {}
+                Ok(_) => {
+                    return Ok(());
+                }
                 Err(e) => {
                     let msg: String = format!("Failed insert error msg: {} {}", msg, e);
                     log::error!("{}", msg);
+                    return Err(e.into());
                 }
             }
-            return Err(e.into());
         }
     };
     // if clientOid in bots entry_id (2 phase)
