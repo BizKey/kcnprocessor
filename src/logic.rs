891c810ae97ad1579a31207be12db4d6ddce1721
kizianov.ao <kizianov.ao@svrauto.ru>
2026-05-14 14:05:13 +0000

    .

--- src/logic.rs
+++ src/logic.rs
@@ -1075,10 +1075,13 @@ pub async fn handle_trade_order_event(order: OrderData, pool: &sqlx::Pool<sqlx::
                             let msg: String = format!("Failed update_bot_balance_by_entry_client_oid: {}", e);
                             log::error!("{}", msg);
                             match insert_db_error(pool, exchange, &msg).await {
-                                Ok(_) => {}
+                                Ok(_) => {
+                                    return Ok(());
+                                }
                                 Err(e) => {
                                     let msg: String = format!("Failed insert error msg: {} {}", msg, e);
                                     log::error!("{}", msg);
+                                    return Err(e.into());
                                 }
                             }
                         }
