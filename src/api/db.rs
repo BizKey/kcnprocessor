e4de5b947805547b9441948227980ebe94bc7176
kizianov.ao <kizianov.ao@svrauto.ru>
2026-07-21 11:08:45 +0000

    .

--- src/api/db.rs
+++ src/api/db.rs
@@ -771,11 +771,7 @@ pub async fn upsert_position_asset(pool: &sqlx::PgPool, asset_symbol: &str, asse
     Ok(())
 }
 pub async fn handle_db_error(pool: &sqlx::PgPool, error_msg: &str) -> () {
-    match insert_db_error(pool, error_msg).await {
-        Ok(_) => (),
-        Err(db_err) => {
-            let msg: String = format!("Failed to insert error to DB: {} | Original: {}", db_err, error_msg);
-            log::error!("{}", msg);
-        }
+    if let Err(db_err) = insert_db_error(pool, error_msg).await {
+        log::error!("Failed to insert error to DB: {db_err} | Original: {error_msg}");
     };
 }
