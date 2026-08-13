ea29c7ee3716c92b3e33b705c01fb8cccd493f8b
kizianov.ao <kizianov.ao@svrauto.ru>
2026-08-13 12:52:03 +0000

    .

---
+++ src/cleanup.rs
@@ -0,0 +1,113 @@
+use anyhow::Result;
+use micromap::Map;
+use sqlx::PgPool;
+use tokio::time::sleep;
+use tracing::{error, info};
+
+use crate::api::requests::{
+    api_v3_hf_margin_stop_order_cancel_by_id_delete, api_v3_hf_margin_stop_orders_get,
+    build_query_string,
+};
+use crate::constants::DELETE_STOP_ORDER_DELAY;
+use crate::logic::auto_clean_account;
+
+pub async fn cancel_all_stop_orders() -> Result<()> {
+    loop {
+        let mut query_params: Map<&str, &str, 8> = Map::new();
+        query_params.insert("pageSize", "10");
+
+        let query_params = match build_query_string(query_params) {
+            Ok(query_params) => query_params,
+            Err(e) => {
+                error!("{:#}", e);
+                continue;
+            }
+        };
+
+        let open_stop_orders = match api_v3_hf_margin_stop_orders_get(&query_params).await {
+            Ok(open_stop_orders) => open_stop_orders,
+            Err(e) => {
+                error!("{:#}", e);
+                continue;
+            }
+        };
+
+        let open_stop_orders = match open_stop_orders {
+            Some(open_stop_orders) => {
+                info!(
+                    "Stop orders: current_page:{} page_size:{} total_num:{} total_page:{}",
+                    open_stop_orders.current_page,
+                    open_stop_orders.page_size,
+                    open_stop_orders.total_num,
+                    open_stop_orders.total_page
+                );
+                open_stop_orders
+            }
+            None => {
+                error!("Fail get list open stop orders:None");
+                continue;
+            }
+        };
+
+        if open_stop_orders.total_num == 0 {
+            info!("All stop orders closed");
+            break;
+        }
+
+        for stop_order in open_stop_orders.items {
+            info!("Stop order:{}", stop_order);
+
+            let mut query_params: Map<&str, &str, 8> = Map::new();
+            query_params.insert("orderId", &stop_order.id);
+
+            let query_params = match build_query_string(query_params) {
+                Ok(query_params) => query_params,
+                Err(e) => {
+                    error!("{:#}", e);
+                    continue;
+                }
+            };
+
+            let canceled_stop_order =
+                match api_v3_hf_margin_stop_order_cancel_by_id_delete(&query_params).await {
+                    Ok(canceled_stop_order) => canceled_stop_order,
+                    Err(e) => {
+                        error!("{:#}", e);
+                        continue;
+                    }
+                };
+
+            let canceled_stop_order = match canceled_stop_order {
+                Some(canceled_stop_order) => canceled_stop_order,
+                None => {
+                    error!("Cancel stop order:{} None", &stop_order.id);
+                    continue;
+                }
+            };
+
+            for st_order in canceled_stop_order.cancelled_order_ids {
+                info!("Success cancel stop order:{}", st_order)
+            }
+        }
+        sleep(DELETE_STOP_ORDER_DELAY).await;
+    }
+
+    Ok(())
+}
+
+pub async fn clean_account(pool: &PgPool) -> Result<()> {
+    loop {
+        let is_completed = match auto_clean_account(pool).await {
+            Ok(is_completed) => is_completed,
+            Err(e) => {
+                error!("{:#}", e);
+                continue;
+            }
+        };
+        if is_completed {
+            info!("auto_clean_account success");
+            break;
+        }
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,27 @@ mod api {
 mod constants;
 mod logic;
 use crate::constants::*;
+mod cleanup;
 mod tracing_layer;
 mod websocket;
 
 use crate::api::db::wipe_bots_info;
-use crate::api::requests::{
-    api_v1_bullet_private_post, api_v3_hf_margin_stop_order_cancel_by_id_delete,
-    api_v3_hf_margin_stop_orders_get, build_query_string,
-};
+
 use crate::api::tools::get_env;
-use crate::logic::{auto_clean_account, create_init_orders, spawn_process_kcn_msg};
+use crate::cleanup::{cancel_all_stop_orders, clean_account};
+use crate::logic::create_init_orders;
 use crate::tracing_layer::DbErrorLayer;
 use crate::websocket::run_websocket_loop;
 use anyhow::Result;
-use bytes::Bytes;
-use dotenvy::dotenv;
-use futures_util::{SinkExt, StreamExt};
-use micromap::Map;
-use std::sync::mpsc::{Sender, channel};
 
-use tracing::{debug, error, info};
+use dotenvy::dotenv;
+
+use tracing::{error, info};
 use tracing_subscriber::{filter::EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 use sqlx::postgres::PgPoolOptions;
-use tokio::sync::mpsc;
-use tokio::time::{Duration, interval, sleep};
-use tokio_tungstenite::{connect_async, tungstenite::Message};
+
+use tokio::time::{Duration, sleep};
 
 fn init_tracing(pool: sqlx::PgPool) {
     let fmt_layer = tracing_subscriber::fmt::layer()
@@ -81,106 +76,14 @@ async fn main() -> Result<()> {
         }
     }
 
-    loop {
-        let mut query_params: Map<&str, &str, 8> = Map::new();
-        query_params.insert("pageSize", "10");
-
-        let query_params = match build_query_string(query_params) {
-            Ok(query_params) => query_params,
-            Err(e) => {
-                error!("{:#}", e);
-                continue;
-            }
-        };
-
-        let open_stop_orders = match api_v3_hf_margin_stop_orders_get(&query_params).await {
-            Ok(open_stop_orders) => open_stop_orders,
-            Err(e) => {
-                error!("{:#}", e);
-                continue;
-            }
-        };
-
-        let open_stop_orders = match open_stop_orders {
-            Some(open_stop_orders) => {
-                info!(
-                    "Stop orders: current_page:{} page_size:{} total_num:{} total_page:{}",
-                    open_stop_orders.current_page,
-                    open_stop_orders.page_size,
-                    open_stop_orders.total_num,
-                    open_stop_orders.total_page
-                );
-                open_stop_orders
-            }
-            None => {
-                error!("Fail get list open stop orders:None");
-                continue;
-            }
-        };
-
-        if open_stop_orders.total_num == 0 {
-            info!("All stop orders closed");
-            break;
-        }
-
-        for stop_order in open_stop_orders.items {
-            info!("Stop order:{}", stop_order);
-
-            let mut query_params: Map<&str, &str, 8> = Map::new();
-            query_params.insert("orderId", &stop_order.id);
-
-            let query_params = match build_query_string(query_params) {
-                Ok(query_params) => query_params,
-                Err(e) => {
-                    error!("{:#}", e);
-                    continue;
-                }
-            };
-
-            let canceled_stop_order =
-                match api_v3_hf_margin_stop_order_cancel_by_id_delete(&query_params).await {
-                    Ok(canceled_stop_order) => canceled_stop_order,
-                    Err(e) => {
-                        error!("{:#}", e);
-                        continue;
-                    }
-                };
-
-            let canceled_stop_order = match canceled_stop_order {
-                Some(canceled_stop_order) => canceled_stop_order,
-                None => {
-                    error!("Cancel stop order:{} None", &stop_order.id);
-                    continue;
-                }
-            };
-
-            for st_order in canceled_stop_order.cancelled_order_ids {
-                info!("Success cancel stop order:{}", st_order)
-            }
-        }
-        sleep(DELETE_STOP_ORDER_DELAY).await;
+    if let Err(e) = cancel_all_stop_orders().await {
+        error!("Failed to cancel stop orders: {:#}", e);
     }
 
     // repay all liability assets and sell
-    loop {
-        let is_completed = match auto_clean_account(&pool).await {
-            Ok(is_completed) => is_completed,
-            Err(e) => {
-                error!("{:#}", e);
-                continue;
-            }
-        };
-        if is_completed {
-            info!("auto_clean_account success");
-            break;
-        }
+    if let Err(e) = clean_account(&pool).await {
+        error!("Failed to clean account: {:#}", e);
     }
-
-    let (tx_in, rx_in) = mpsc::channel::<Bytes>(8192);
-
-    let pool_process = pool.clone();
-    let _spawn_process_kcn_msg_point =
-        tokio::spawn(async move { spawn_process_kcn_msg(&pool_process, rx_in).await });
 
     if !init_order_execute {
         let pool_init_orders = pool.clone();
